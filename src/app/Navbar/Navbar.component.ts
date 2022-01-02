938393831a0fe74b57f5bd360ed511fdb952dd86
Laskev <al.laskevic@gmail.com>
2022-01-02 21:30:49 +0100

    Cookie-Updates

--- src/app/Navbar/Navbar.component.ts
+++ src/app/Navbar/Navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-Navbar',
@@ -11,20 +12,26 @@ export class NavbarComponent implements OnInit {
   static auto:number = 1;
   static freq:number = 100;
   static elon:boolean = false;
+  savecoins:string="";
   interval :any;
 
   @HostListener('document:click', ['$event'])
   clickout(event : any) {
     if(this.eRef.nativeElement.contains(event.target)) {
       NavbarComponent.dogeCoin = NavbarComponent.dogeCoin + NavbarComponent.multiplier + 1 * NavbarComponent.multiplier;
+      this.savecoins = NavbarComponent.toString();
+      this.cookie.set("dogeCoin", this.savecoins);
     }
   }
 
-   constructor(private eRef: ElementRef) {
+   constructor(private eRef: ElementRef, private cookie: CookieService) {
     NavbarComponent.dogeCoin = 0;
   }
 
   ngOnInit() {
+    if(this.cookie.check("dogeCoin"))
+      NavbarComponent.dogeCoin = +this.cookie.get("dogeCoin");
+    
     this.interval = setInterval(() => {
       NavbarComponent.dogeCoin = NavbarComponent.dogeCoin + NavbarComponent.auto;
   },NavbarComponent.freq)
